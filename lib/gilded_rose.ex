58b8e7300099411b1a44e827b1d175bb5943aaa1
Makis Otman <makisotman@gmail.com>
2015-09-18 16:55:01 +0100

    Replace pipes

--- lib/gilded_rose.ex
+++ lib/gilded_rose.ex
@@ -4,11 +4,7 @@ defmodule GildedRose do
   # => [%Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 9, quality: 3}]
 
   def update_quality(items) do
-    items
-    |> Enum.map(fn(item) ->
-      item
-      |> update_item
-    end)
+    Enum.map(items, &update_item/1)
   end
 
   def update_item(item) do
